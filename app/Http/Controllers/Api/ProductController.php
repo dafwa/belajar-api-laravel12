003d909f1d8acbf463fa84a0d72df70deae50a67
dafwa <122160658+dafwa@users.noreply.github.com>
2025-12-30 15:21:22 +0800

    Add store method in ProductController

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -21,5 +22,44 @@ class ProductController extends Controller
 
         // return collection of products as a resource
         return new ProductResource(true, 'List Data Products', $products);
+    }
+
+    /**
+     * store
+     * 
+     * @param mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        // define validation rules
+        $validator = Validator::make($request->all(), [
+            'image'         => 'required|image|mimes:png,jpg,jpeg,gif,svg|max:2048',
+            'title'         => 'required',
+            'description'   => 'required',
+            'price'         => 'required|numeric',
+            'stock'         => 'required|numeric',
+        ]);
+
+        // check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('products', $image->hashName());
+
+        // create product
+        $product = Product::create([
+            'image'         => $image->hashName(),
+            'title'         => $request->title,
+            'description'   => $request->description,
+            'price'         => $request->price,
+            'stock'         => $request->stock,
+        ]);
+
+        // return response
+        return new ProductResource(true, 'Data Product Berhasil Ditambahkan!', $product);
     }
 }
