8086b7570245d064998893746d4f9fb2dce1034b
dafwa <122160658+dafwa@users.noreply.github.com>
2025-12-30 14:39:52 +0800

    Add index method for ProductController

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * index
+     * 
+     * @return void
+     */
+    public function index()
+    {
+        // get all products
+        $products = Product::latest()->paginate(5);
+
+        // return collection of products as a resource
+        return new ProductResource(true, 'List Data Products', $products);
+    }
 }
