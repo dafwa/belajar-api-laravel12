f7d8ca45226cf8d730912131ba41495c2c01d470
dafwa <122160658+dafwa@users.noreply.github.com>
2025-12-30 15:03:43 +0800

    Added /products route to api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// products
+Route::apiResource('/products', ProductController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
